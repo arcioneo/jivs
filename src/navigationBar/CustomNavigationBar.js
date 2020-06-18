32d9f03a0d34dcef5dd1b809039188062a14e875
Eduardo Milpas <blaseduardo.milpasdiaz@mastercard.com>
2020-06-17 23:11:32 -0400

    adding auto close in the menu

--- src/navigationBar/CustomNavigationBar.js
+++ src/navigationBar/CustomNavigationBar.js
@@ -1,19 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './customNavigationBar.css';
 import logo from '../images/logo.png';
 
 export default function CustomNavigationBar(props) {
+  const [expanded, setExpanded] = useState(false);
+
   function menuClicked(componentClicked) {
+    setExpanded(false)
     props.menuClicked(componentClicked)
   }
 
+
+
   return(
     <div id="customNavigationBar">
-      <Navbar bg="light" variant="light" expand="sm">
+      <Navbar bg="light" variant="light" expand="sm" expanded={expanded}>
         <Navbar.Brand href="#"><img src={logo} width="60px" alt="" />Grupo SEJIV</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded ? false : "expanded")} />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
             <Nav.Link onClick={() => menuClicked('aboutUs')}>Qui&eacute;nes somos</Nav.Link>
